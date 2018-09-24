0a61620405b4eea252971993ab51e7a5a349cf65
anne-other <anne.c.ince@gmail.com>
2018-09-24 16:17:34 +0100

    Calculator can subtract

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     Calculator calculator;
@@ -6,6 +9,18 @@ public class CalculatorTest {
     @Before
     public void before(){
         calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        int addition = calculator.add(2, 3);
+        assertEquals(5, addition);
+    }
+
+    @Test
+    public void canSubtract(){
+        int subtraction = calculator.subtract(3, 2);
+        assertEquals(1, subtraction);
     }
 
 }
